5dcd80073429c2fa8f7593c674dcb594e89161bd
Leesunwoo <x21999@naver.com>
2018-11-04 15:31:21 +0900

    Wrtting Files..

--- generator.py
+++ generator.py
@@ -52,6 +52,8 @@ ap.add_argument("-k", "--kind", type=str, default=None,
 	help="type of specification to be done")
 ap.add_argument("-n", "--num", type=int, default=1,
 	help="Number of Car plate Data")
+ap.add_argument("-t", "--type", type=str, default='RPA',
+	help="Number of Car plate Data")
 
 args = vars(ap.parse_args())
 folder = args["dir"]
@@ -62,16 +64,21 @@ fir_pas = ['%02d'%i for i in range(1, 70)]
 fir_van = ['%02d'%i for i in range(70, 80)]
 fir_exp = ['%02d'%i for i in range(80,98)]
 fir_spe = ['%02d'%i for i in range(98, 100)]
+fir_list=[fir_pas,fir_van, fir_exp, fir_spe]
+
+def return_image(number):
+    digits_path = 'image/Number/'+number+'.png'
+    return cv2.imread(digits_path)
 
 def return_2image(number):
-    # i_str = '{:02}'.format(number)
+    i_str = '{:02}'.format(number)
     i_str=number
     return (return_image(i_str[0]), return_image(i_str[1]))
 
-fir_pas_dic = [return_2image(i) for i in range(1, 70)]    
-fir_van_dic = [return_2image(i) for i in range(70, 80)]
-fir_exp_dic = [return_2image(i) for i in range(80,98)]
-fir_spe_dic = [return_2image(i) for i in range(98, 100)]
+# fir_pas_dic = [return_2image(i) for i in range(1, 70)]    
+# fir_van_dic = [return_2image(i) for i in range(70, 80)]
+# fir_exp_dic = [return_2image(i) for i in range(80,98)]
+# fir_spe_dic = [return_2image(i) for i in range(98, 100)]
 
 diploma_dic = {'국기':cv2.imread('image/Diploma/gukgi.png'),
                         '협정/대표':cv2.imread('image/Diploma/hyeopjeong_datepyeo.png'),
@@ -121,26 +128,73 @@ sec_rent_dic={'하':cv2.imread('image/Rent/ha.png'),
                         '호':cv2.imread('image/Rent/ho.png')}
 #Third Part
 thir_num = ['%04d'%i for i in range(0, 10000)]
-def return_image(number):
-    digits_path = 'image/Number/'+str(number)+'.png'
-    return cv2.imread(digits_path)
+
 
 #License Plate Type.
-plate_type={'type1':cv2.imread('image/license_plates_template/type_1.gif'),
-                    'type2':cv2.imread('image/license_plates_template/type_2.gif'),
-                    'type3':cv2.imread('image/license_plates_template/type_3.jpg'),
-                    'type4':cv2.imread('image/license_plates_template/type_4.jpg'),
-                    'type5':cv2.imread('image/license_plates_template/type_5.jpg')}
+plate_type={'type1':cv2.imread('image/license_plates_template/type1.jpg'),
+                    'type2':cv2.imread('image/license_plates_template/type2.jpg'),
+                    'type3':cv2.imread('image/license_plates_template/type3.jpg'),
+                    'type4':cv2.imread('image/license_plates_template/type4.jpg'),
+                    'type5':cv2.imread('image/license_plates_template/type5.jpg')}
+#Plate type consis of 335x115, 520x110, 440x220, 335x170, 335x155
+"""*Personal or Non-Commercial (Total 32)
+Type                                    Before 2006             After 2006
+Regular : Personal             335x170(type5)     335x155(type2), 520x110(type1)
+                Commercial          335x170(type3)      335x170(type3), 520x110(type3)
+Large        : Personal         440x220(type5)      440x220(type2)
+Vehicles  : Commercial       440x220(type3)     440x220(type3)
+Rental Cars :                       335x170(type5)      335x155(type2), type1(520x110)
+"""
+#type be Combination of  R(Regular) + P(personal) or Commercial + Before or After , 
+#                                      L(Large)+P(Personal) or C(Commercial) + Before or After,
+#                                      R(Rental) +Before or After
+#RPB is mean Regular Personal Before(2006)
+plate_size_color = { 'RPB':[[(335,170), 'type5']],'RPA':[[(335,155), 'type2'],[(520,110),'type1']],
+                                  'RCB':[[(335,170), 'type3']], 'RCA':[[(335,170), 'type3'], [(520,110), 'type3']],
+                                  'LPB':[[(440,220), 'type5']], 'LPA':[[(440,220), 'type2']],
+                                  'LCB':[[(440,220), 'type3']], 'LCA':[[(440,220), 'type3']],
+                                  'RB':[[(335,170), 'type5']],'RA':[[(355,155), 'type2'],[(520,110), 'type1']]}
 
-def combine(first, second, thrid):
+def write_img(name, image):
+    print("Saved..\n %s%s.jpg" % folder, name)
+    #존재하면 다른이름.
+    cv2.imwrite(folder+name+'.jpg', image)
+
+def selectPlateType(name, ptype, firstImg, secondImg, thirdImg):
+    print(ptype)
+    psc = plate_size_color[ptype]
+    for i in psc:
+        img = plate_type[i[1]]  #type Select
+        img = cv2.resize(img, i[0]) #Resizing Image
+        # cv2.imshow('test', img) 
+        f = cv2.hconcat(firstImg)
+        t = cv2.hconcat(thirdImg)
+        res = cv2.hconcat([f,secondImg, t])
+        res = cv2.resize(res, (319,83))
+        #46.4에서 시작
+        # cv2.imshow('test', res)
+        # while cv2.waitKey(10)==ord('q'):
+        #     continue
+        # Write Image
+        cv2.imwrite("example.png", res)
+        # write_img(name, res)
+
+def combine(first, second, third, ptype):
     print('Combine')
-    if first ==None or second==None or thrid==None:
+    name = first+second+third
+    print(name)
+    if first ==None or second==None or third==None:
         print("None Value Exists...")
         exit()
+    #Two List
+    firstImg = [return_image(i) for i in first]
+    #One Image
+    secondImg = sec_per_dic[second]
+    #Four List
+    thirdImg = [return_image(i) for i in third]
+    selectPlateType(name, ptype, firstImg, secondImg, thirdImg)
+    print (first, second, third)
     
-    
-
-
 def main():
     if os.path.isdir(folder):
         if os.path.exists(os.getcwd()+folder):
@@ -152,21 +206,37 @@ def main():
         os.mkdir(folder)
 
     for i in range(0,num):
+        first = None
+        second=None
+        third=None
         if args['kind'] =='personal':
             print('personal')
-            print(random.choice(fir_pas), random.choice(sec_per), random.choice(thir_num))
+            first = random.choice(random.choice(fir_list))
+            second = random.choice(sec_per)
+            third = random.choice(thir_num)
+            print (first, second, third)
         elif args['kind'] =='Taxi' or args['kind']=='bus':
             print('Taxi or Bus')
-            print(random.choice(fir_pas),random.choice(sec_tab),random.choice(thir_num))
+            first = random.choice(random.choice(fir_list))
+            second = random.choice(sec_per)
+            third = random.choice(thir_num)
+            print (first, second, third)
         elif args['kind']=='Express':
             print('Express')
-            print(random.choice(fir_pas),random.choice(sec_exp),random.choice(thir_num))
+            first = random.choice(random.choice(fir_list))
+            second = random.choice(sec_per)
+            third = random.choice(thir_num)
+            print (first, second, third)
         elif args['kind'] == 'Rent':
             print('Rent')
-            print(random.choice(fir_pas),random.choice(sec_rent),random.choice(thir_num))
+            first = random.choice(random.choice(fir_list))
+            second = random.choice(sec_per)
+            third = random.choice(thir_num)
+            print (first, second, third)
+        combine(first, second, third, args['type'])
 
 if __name__ == '__main__':
     if len(sys.argv) < 7:
         print("Please Command\n Example \n -d 'directory' -k 'kind', -n 'numbers' ")
-    exit()
+        exit()
     main()
