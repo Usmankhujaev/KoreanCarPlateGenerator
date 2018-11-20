54e1345ea465a9aaaa2e35537a276b8472de7246
Leesunwoo <x21999@naver.com>
2018-11-20 11:47:36 +0900

    Bug Fixed. digit_list delted

--- licensePlateType.py
+++ licensePlateType.py
@@ -4,7 +4,16 @@ import random
 import codecs
 import argparse
 # -*- coding: utf-8-sig-*-
-
+'''
+Region = {"A": "서울 ", "B": "경기 ", "C": "인천 ", "D": "강원 ", "E": "충남 ", "F": "대전 ",
+                                "G": "충북 ", "H": "부산 ", "I": "울산 ", "J": "대구 ", "K": "경북 ", "L": "경남 ",
+                                "M": "전남 ", "N": "광주 ", "O": "전북 ", "P": "제주 "}
+Hangul = {"dk": "아", "dj": "어", "dh": "오", "dn": "우", "qk": "바", "qj": "버", "qh": "보", "qn": "부",
+                        "ek": "다", "ej": "더", "eh": "도", "en": "두", "rk": "가", "rj": "거", "rh": "고", "rn": "구",
+                        "wk": "자", "wj": "저", "wh": "조", "wn": "주", "ak": "마", "aj": "머", "ah": "모", "an": "무",
+                        "sk": "나", "sj": "너", "sh": "노", "sn": "누", "fk": "라", "fj": "러", "fh": "로", "fn": "루",
+                        "tk": "사", "tj": "서", "th": "소", "tn": "수", "gj": "허"}
+'''
 class LicensePlate:
     def __init__(self):
         self.img_width = None
@@ -14,8 +23,22 @@ class LicensePlate:
         self.pType = None
         self.city_path=None
         self.pTypelist = ["RPA1", "RPA2", "RCA1", "RCA2", "LVPA1", "LVCA1"]
-        self.digit_list=[]
         self.training_dir = "image/train/"
+        self.Region = {"서울":"A", "경기":"B", "인천":"C", "강원":"D", "충남":"E", "대전":"F",
+                                "충북":"G", "부산":"H", "울산":"I", "대구":"J", "경북":"K", "경남":"L",
+                                "전남":"M", "광주":"N", "전북":"O", "제주":"P"}
+
+        self.kongToHangul = {"a":"dk", "eo":"dj", "o":"dh", "u":"dn", "ba":"qk", "beo":"qj", "bo":"qh", "bu":"qn",
+                                "da":"ek", "deo":"ej", "do":"eh", "du":"en", "ga":"rk", "geo":"rj", "go":"rh", "gu":"rn",
+                                "ja":"wk", "jeo":"wj", "jo":"wh", "ju":"wn", "ma":"ak", "meo":"aj", "mo":"ah", "mu":"an",
+                                "na":"sk", "neo":"sj", "no":"sh", "nu":"sn", "la":"fk", "leo":"fj", "lo":"fh", "lu":"fn",
+                                "sa":"tk", "seo":"tj", "so":"th", "su":"tn", "heo":"gj","ha":"gk","ho":"gh", "reo":"fj"}
+
+        self.dictToHangul = {"dk": "아", "dj": "어", "dh": "오", "dn": "우", "qk": "바", "qj": "버", "qh": "보", "qn": "부",
+                                "ek": "다", "ej": "더", "eh": "도", "en": "두", "rk": "가", "rj": "거", "rh": "고", "rn": "구",
+                                "wk": "자", "wj": "저", "wh": "조", "wn": "주", "ak": "마", "aj": "머", "ah": "모", "an": "무",
+                                "sk": "나", "sj": "너", "sh": "노", "sn": "누", "fk": "라", "fj": "러", "fh": "로", "fn": "루",
+                                "tk": "사", "tj": "서", "th": "소", "tn": "수", "gj": "허"}
 
     def regularPersonalAfter1(self):
         # Width 335mm - 1081 px
@@ -190,21 +213,21 @@ class LicensePlate:
                                         (220, 31+self.btn_hg_width+self.num_width*3)]
 
         self.city_path = {
-            "busan":cv2.imread("image/type3/busan.jpg"),
-            "cheonbuk":cv2.imread("image/type3/cheonbuk.jpg"),
-            "chungbuk":cv2.imread("image/type3/chungbuk.jpg"),
-            "chungnam":cv2.imread("image/type3/chungnam.jpg"),
-            "daegu":cv2.imread("image/type3/daegu.jpg"),
-            "daejeon":cv2.imread("image/type3/daejon.jpg"),
-            "gangwon":cv2.imread("image/type3/gangwon.jpg"),
-            "gwangju":cv2.imread("image/type3/gwangju.jpg"),
-            "gyeongi":cv2.imread("image/type3/gyeongi.jpg"),
-            "incheon":cv2.imread("image/type3/incheon.jpg"),
-            "jeju":cv2.imread("image/type3/jeju.jpg"),
-            "kyeongbuk":cv2.imread("image/type3/kyeongbuk.jpg"),
-            "kyeongnam":cv2.imread("image/type3/kyeongnam.jpg"),
-            "seoul":cv2.imread("image/type3/seoul.jpg"),
-            "ulsan":cv2.imread("image/type3/ulsan.jpg")
+            "부산":cv2.imread("image/type3/busan.jpg"),
+            "전북":cv2.imread("image/type3/cheonbuk.jpg"),
+            "충북":cv2.imread("image/type3/chungbuk.jpg"),
+            "충남":cv2.imread("image/type3/chungnam.jpg"),
+            "대구":cv2.imread("image/type3/daegu.jpg"),
+            "대전":cv2.imread("image/type3/daejon.jpg"),
+            "강원":cv2.imread("image/type3/gangwon.jpg"),
+            "광주":cv2.imread("image/type3/gwangju.jpg"),
+            "경기":cv2.imread("image/type3/gyeongi.jpg"),
+            "인천":cv2.imread("image/type3/incheon.jpg"),
+            "제주":cv2.imread("image/type3/jeju.jpg"),
+            "경북":cv2.imread("image/type3/kyeongbuk.jpg"),
+            "경남":cv2.imread("image/type3/kyeongnam.jpg"),
+            "서울":cv2.imread("image/type3/seoul.jpg"),
+            "울산":cv2.imread("image/type3/ulsan.jpg")
         }
         self.digit_path =  {
             "0":cv2.imread("image/type3/0.jpg"),
@@ -253,22 +276,22 @@ class LicensePlate:
                                         (44, 103+self.top_hg_width+self.top_num_width*2+self.btn_hg_width+self.num_width*3)]
         
         self.city_path = {
-            "busan":cv2.imread("image/type4/busan.jpg"),
-            "cheonbuk":cv2.imread("image/type4/cheonbuk.jpg"),
-            "chungbuk":cv2.imread("image/type4/chungbuk.jpg"),
-            "chungnam":cv2.imread("image/type4/chungnam.jpg"),
-            "daegu":cv2.imread("image/type4/daegu.jpg"),
-            "daejeon":cv2.imread("image/type4/daejon.jpg"),
-            "gangwon":cv2.imread("image/type4/gangwon.jpg"),
-            "gwangju":cv2.imread("image/type4/gwangju.jpg"),
-            "gyeongi":cv2.imread("image/type4/gyeongi.jpg"),
-            "incheon":cv2.imread("image/type4/incheon.jpg"),
-            "jeju":cv2.imread("image/type4/jeju.jpg"),
-            "jeonnam":cv2.imread("image/type4/jeonnam.jpg"),
-            "kyeongbuk":cv2.imread("image/type4/kyeongbuk.jpg"),
-            "kyeongnam":cv2.imread("image/type4/kyeongnam.jpg"),
-            "seoul":cv2.imread("image/type4/seoul.jpg"),
-            "ulsan":cv2.imread("image/type4/ulsan.jpg")
+            "부산":cv2.imread("image/type4/busan.jpg"),
+            "전북":cv2.imread("image/type4/cheonbuk.jpg"),
+            "충북":cv2.imread("image/type4/chungbuk.jpg"),
+            "충남":cv2.imread("image/type4/chungnam.jpg"),
+            "대구":cv2.imread("image/type4/daegu.jpg"),
+            "대전":cv2.imread("image/type4/daejon.jpg"),
+            "강원":cv2.imread("image/type4/gangwon.jpg"),
+            "광주":cv2.imread("image/type4/gwangju.jpg"),
+            "경기":cv2.imread("image/type4/gyeongi.jpg"),
+            "인천":cv2.imread("image/type4/incheon.jpg"),
+            "제주":cv2.imread("image/type4/jeju.jpg"),
+            "전남":cv2.imread("image/type4/jeonnam.jpg"),
+            "경북":cv2.imread("image/type4/kyeongbuk.jpg"),
+            "경남":cv2.imread("image/type4/kyeongnam.jpg"),
+            "서울":cv2.imread("image/type4/seoul.jpg"),
+            "울산":cv2.imread("image/type4/ulsan.jpg")
         }
         self.digit_path =  {
             "0":cv2.imread("image/type4/0.jpg"),
@@ -400,21 +423,21 @@ class LicensePlate:
                                         (220, 31+self.btn_hg_width+self.num_width*3)]
 
         self.city_path = {
-            "busan":cv2.imread("image/type6/busan.jpg"),
-            "cheonbuk":cv2.imread("image/type6/jeonbuk.jpg"),
-            "chungbuk":cv2.imread("image/type6/chungbuk.jpg"),
-            "chungnam":cv2.imread("image/type6/chungnam.jpg"),
-            "daegu":cv2.imread("image/type6/daegu.jpg"),
-            "daejeon":cv2.imread("image/type6/daejeon.jpg"),
-            "gangwon":cv2.imread("image/type6/gangwon.jpg"),
-            "gwangju":cv2.imread("image/type6/gwangju.jpg"),
-            "gyeongi":cv2.imread("image/type6/gyeongi.jpg"),
-            "incheon":cv2.imread("image/type6/incheon.jpg"),
-            "jeju":cv2.imread("image/type6/jeju.jpg"),
-            "kyeongbuk":cv2.imread("image/type6/kyeongbuk.jpg"),
-            "kyeongnam":cv2.imread("image/type6/kyeongnam.jpg"),
-            "seoul":cv2.imread("image/type6/seoul.jpg"),
-            "ulsan":cv2.imread("image/type6/ulsan.jpg")
+            "부산":cv2.imread("image/type6/busan.jpg"),
+            "전북":cv2.imread("image/type6/jeonbuk.jpg"),
+            "충북":cv2.imread("image/type6/chungbuk.jpg"),
+            "충남":cv2.imread("image/type6/chungnam.jpg"),
+            "대구":cv2.imread("image/type6/daegu.jpg"),
+            "대전":cv2.imread("image/type6/daejeon.jpg"),
+            "강원":cv2.imread("image/type6/gangwon.jpg"),
+            "광주":cv2.imread("image/type6/gwangju.jpg"),
+            "경기":cv2.imread("image/type6/gyeongi.jpg"),
+            "인천":cv2.imread("image/type6/incheon.jpg"),
+            "제주":cv2.imread("image/type6/jeju.jpg"),
+            "경북":cv2.imread("image/type6/kyeongbuk.jpg"),
+            "경남":cv2.imread("image/type6/kyeongnam.jpg"),
+            "서울":cv2.imread("image/type6/seoul.jpg"),
+            "울산":cv2.imread("image/type6/ulsan.jpg")
         }
         self.digit_path =  {
             "0":cv2.imread("image/type6/0.jpg"),
@@ -449,62 +472,75 @@ class LicensePlate:
     def sortedDigitsOneLine(self):
         img = self.create_image(self.img_height, self.img_width, 3, (255,255,255))
         num_list=[]
+        digit_list=[]
         for i in range(7):
             k, v = random.choice(list(self.digit_path.items()))
             digits = cv2.resize(v, (self.num_width,self.num_height))
-            self.digit_list.append(digits)
+            digit_list.append(digits)
             num_list.append(k)
         hangul_k, hangul_v = random.choice(list(self.hg_path.items()))
-        self.digit_list[2] = cv2.resize(hangul_v, (self.hg_width, self.hg_height))
+        #Dict -> konglish to Hangul
+        hangul_k = self.kongToHangul[hangul_k]
+        digit_list[2] = cv2.resize(hangul_v, (self.hg_width, self.hg_height))
         num_list[2] = hangul_k
         for i in range(7):
             x1, y1 = self.start_coord[i]
-            x2, y2, _ = self.digit_list[i].shape
-            img[x1:x2+x1, y1:y2+y1] = self.digit_list[i]
+            x2, y2, _ = digit_list[i].shape
+            img[x1:x2+x1, y1:y2+y1] = digit_list[i]
         return self.training_dir+''.join(num_list), img
     
     def sortedDigitsTwoLine(self):
         img = self.create_image(self.img_height, self.img_width, 3, (255,255,255))
         num_list=[]
+        digit_list=[]
         for i in range(8):
             k, v = random.choice(list(self.digit_path.items()))
             digits = cv2.resize(v, (self.num_width,self.num_height))
-            self.digit_list.append(digits)
+            digit_list.append(digits)
             num_list.append(k)
         hangul_k, hangul_v = random.choice(list(self.hg_path.items()))
+        #Dict -> konglish to Hangul
+        hangul_k = self.kongToHangul[hangul_k]
         city_k, city_v = random.choice(list(self.city_path.items()))
-        self.digit_list[0] = cv2.resize(city_v, (self.top_hg_width, self.top_hg_height))
-        self.digit_list[1] = cv2.resize(self.digit_list[1], (self.top_num_width, self.top_num_height))
-        self.digit_list[2] = cv2.resize(self.digit_list[2], (self.top_num_width, self.top_num_height))
-        self.digit_list[3] = cv2.resize(hangul_v, (self.btn_hg_width, self.btn_hg_height))
+        digit_list[0] = cv2.resize(city_v, (self.top_hg_width, self.top_hg_height))
+        digit_list[1] = cv2.resize(digit_list[1], (self.top_num_width, self.top_num_height))
+        digit_list[2] = cv2.resize(digit_list[2], (self.top_num_width, self.top_num_height))
+        digit_list[3] = cv2.resize(hangul_v, (self.btn_hg_width, self.btn_hg_height))
+        #City To BIG Eng
+        city_k = self.Region[city_k]
         num_list[0] = city_k
         num_list[3] = hangul_k
         for i in range(8):
             x1, y1 = self.start_coord[i]
-            x2, y2, _ = self.digit_list[i].shape
-            img[x1:x2+x1, y1:y2+y1] = self.digit_list[i]
+            x2, y2, _ = digit_list[i].shape
+            img[x1:x2+x1, y1:y2+y1] = digit_list[i]
         return self.training_dir+''.join(num_list), img
 
     def sortedDigitsOneLineCity(self):
         img = self.create_image(self.img_height, self.img_width, 3, (255,255,255))
         num_list=[]
+        digit_list=[]
         for i in range(8):
             k, v = random.choice(list(self.digit_path.items()))
             digits = cv2.resize(v, (self.num_width,self.num_height))
-            self.digit_list.append(digits)
+            digit_list.append(digits)
             num_list.append(k)
         hangul_k, hangul_v = random.choice(list(self.hg_path.items()))
+        #Dict -> konglish to Hangul
+        hangul_k = self.kongToHangul[hangul_k]
         city_k, city_v = random.choice(list(self.city_path.items()))
-        self.digit_list[0] = cv2.resize(city_v, (self.top_hg_width, self.top_hg_height))
-        self.digit_list[1] = cv2.resize(self.digit_list[1], (self.top_num_width, self.top_num_height))
-        self.digit_list[2] = cv2.resize(self.digit_list[2], (self.top_num_width, self.top_num_height))
-        self.digit_list[3] = cv2.resize(hangul_v, (self.btn_hg_width, self.btn_hg_height))
+        #City To BIG Eng
+        city_k = self.Region[city_k]
+        digit_list[0] = cv2.resize(city_v, (self.top_hg_width, self.top_hg_height))
+        digit_list[1] = cv2.resize(digit_list[1], (self.top_num_width, self.top_num_height))
+        digit_list[2] = cv2.resize(digit_list[2], (self.top_num_width, self.top_num_height))
+        digit_list[3] = cv2.resize(hangul_v, (self.btn_hg_width, self.btn_hg_height))
         num_list[0] = city_k
         num_list[3] = hangul_k
         for i in range(8):
             x1, y1 = self.start_coord[i]
-            x2, y2, _ = self.digit_list[i].shape
-            img[x1:x2+x1, y1:y2+y1] = self.digit_list[i]
+            x2, y2, _ = digit_list[i].shape
+            img[x1:x2+x1, y1:y2+y1] = digit_list[i]
         return self.training_dir+''.join(num_list), img
 
     def generatePlate(self, pType):
@@ -517,6 +553,7 @@ class LicensePlate:
             saveStr, img = self.sortedDigitsOneLine()
         elif pType == "RCA1":
             self.regularCommercialAfter1()
+            saveStr, img = self.sortedDigitsTwoLine()
         elif pType == "RCA2":
             self.regularCommercialAfter2()
             saveStr, img = self.sortedDigitsTwoLine()
@@ -544,12 +581,9 @@ if __name__ == '__main__':
     if args["type"] >6 or args["type"] < 1:
         print("Type is unvalid!")
         exit()
-    import sys
-    print(sys.stdin.encoding)
     if args["train"] == "train":
         main.training_dir = "image/train/"
         main.training_dir = main.training_dir + 'type'+ str(args["type"])+'/'
-        print(main.training_dir)
     elif args["train"] == "test":
         main.training_dir = "image/test/"
         main.training_dir = main.training_dir + 'type'+ str(args["type"])+'/'
